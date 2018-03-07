15a174d0e907b2c06c18de7a2124cb81fc6c54a1
Oyvind Moldestad <moldestad@gmail.com>
2018-03-07 17:51:44 +0100

    Initial commit

---
+++ src/main/java/no/capraconsulting/ks2kurs2018/Ks2Kurs2018Application.java
@@ -0,0 +1,12 @@
+package no.capraconsulting.ks2kurs2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ks2Kurs2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ks2Kurs2018Application.class, args);
+	}
+}
